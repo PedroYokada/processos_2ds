ea29e0151fec01e58d02fd312174527a1bde3616
PedroYokada <pedro_hlyokada@outlook.com>
2025-05-08 07:33:06 -0300

    Adicionando exemplos semana 10 aulas 1 processos

---
+++ Processos semana 10 aula 1/termometro.py
@@ -0,0 +1,21 @@
+class Termometro:
+    def __init__(self, temperatura=0):
+        self.__temperatura_celsius = temperatura
+    
+    def get_temperatura_celsius(self):
+        return self.__temperatura_celsius
+    
+    def get_temperatura_fahrenheit(self):
+        return (self.__temperatura_celsius * 9/5) + 32
+    
+    def set_temperatura_celsius(self, temperatura):
+        self.__temperatura_celsius = temperatura
+    
+    def set_temperatura_fahrenheit(self, temperatura):
+        self.__temperatura_celsius = (temperatura - 32) * 5/9
+
+# Uso da classe
+termometro = Termometro()
+termometro.set_temperatura_fahrenheit(68)
+print(termometro.get_temperatura_fahrenheit())  # Deve mostrar 68
+print(termometro.get_temperatura_celsius())     # Mostra a temperatura em Celsius
